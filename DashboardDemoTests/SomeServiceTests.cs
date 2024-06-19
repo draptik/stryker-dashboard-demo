fdcde45683911295be9a79cb459271b3ddb008a7
Patrick Drechsler <patrick@pdrechsler.de>
2024-06-19 22:38:59 +0200

    🧪 test: add failing test

--- DashboardDemoTests/SomeServiceTests.cs
+++ DashboardDemoTests/SomeServiceTests.cs
@@ -8,5 +8,6 @@ public class SomeServiceTests
     [Theory]
     [InlineData(10, false)]
     [InlineData(20, true)]
+    [InlineData(30, false)]
     public void IsAdultWorks(int age, bool expected) => SomeService.IsAdult(age).Should().Be(expected);
 }
